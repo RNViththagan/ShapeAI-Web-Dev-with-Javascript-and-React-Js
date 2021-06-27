cbac609a6dfffb99d0a9a8680c0c62a009ec24b9
Viththagan R.N <84705898+RNViththagan@users.noreply.github.com>
2021-06-27 08:18:04 +0530

    project uploaded

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header.jsx";
+import Info from "./Info.jsx";
+import Footer from "./Footer.jsx";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
